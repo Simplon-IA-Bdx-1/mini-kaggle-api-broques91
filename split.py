a4a4db00892a333b2cb409be63ffdff160e26ea2
Bastien <bastien@macbook-pro-de-bastien-1.home>
2020-07-29 22:04:40 +0200

    get training data from remote

--- split.py
+++ split.py
@@ -1,20 +1,32 @@
 import pandas as pd
+import os 
 
+# Create Data Directory
+path = "./data"
+
+try:
+    os.makedirs(path)
+    print ("Successfully created the directory %s" % path)
+except FileExistsError: 
+    print("Directory " , path ,  " already exists")  
+else:
+    print ("Creation of the directory %s failed" % path)
 
 # Load Dataset
-df = pd.read_csv('data/cs-training.csv', sep=',', index_col=0)
+df = pd.read_csv('https://oml-data.s3.amazonaws.com/kaggle-give-me-credit-train.csv', index_col=0)
 df = df.dropna()
-print(df.columns)
+# print(df.columns)
 
 # Split Dataset
 train_set = df.sample(frac=0.8, random_state=42)
 test_set = df.drop(train_set.index)
-test_set2 = test_set.copy()
-test_set2['SeriousDlqin2yrs'] = ''
+
+print("######## Split complete #########")
+print(f"Train set : {len(train_set)} entries")
+print(f"Test set : {len(test_set)} entries")
 
 # Save New Datasets
-train_set.to_csv('data/train.csv')
-test_set.to_csv('data/test.csv')
-test_set2.to_csv('data/test2.csv')
+train_set.to_csv('data/train2.csv')
+test_set.to_csv('data/test2.csv')
 
 
