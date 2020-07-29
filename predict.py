be031d095f34d1c190094f81979cb6ce48cc4901
Bastien <bastien@macbook-pro-de-bastien-1.home>
2020-07-29 22:09:26 +0200

    rename model var

--- predict.py
+++ predict.py
@@ -4,28 +4,28 @@ import pandas as pd
 import joblib
 
 # Load Dataset
-df_train = pd.read_csv('data/train.csv', sep=',', index_col=0)
-df_test = pd.read_csv('data/test.csv', sep=',', index_col=0)
-X_test = df_test.drop(['SeriousDlqin2yrs'], axis=1)
+df_train = pd.read_csv('data/train2.csv', sep=',', index_col=0)
+df_test = pd.read_csv('data/test2.csv', sep=',', index_col=0)
 
 # Features / Target
-x = df_train.drop('SeriousDlqin2yrs', axis=1)
-y = df_train['SeriousDlqin2yrs']
-
-# Train Test Split
-X_train, X_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+X_train = df_train.drop('SeriousDlqin2yrs', axis=1)
+y_train = df_train['SeriousDlqin2yrs']
+X_test = df_test.drop(['SeriousDlqin2yrs'], axis=1)
+y_test = df_test['SeriousDlqin2yrs']
 
 # Train Classifier
-tree_clf = DecisionTreeClassifier()
-tree_clf.fit(X_train,y_train)
+model = DecisionTreeClassifier()
+model.fit(X_train,y_train)
 
 # Predict
-y_pred = tree_clf.predict(X_test)
-df_test['SeriousDlqin2yrs'] = y_pred
+y_pred = model.predict_proba(X_test)[:,1]
 
-# Export Predictions
-df_test.to_csv('test2-predictions.csv')
+# DataFrame Predictions
+data = {'SeriousDlqin2yrs': y_pred}
+predictions = pd.DataFrame(data=data)
+
+# Save Predictions
+predictions.to_csv('test2-predictions.csv')
 
 # Save Classifier
-joblib.dump(tree_clf, 'model/model.pkl')
-
+joblib.dump(model, 'model/model.pkl')
